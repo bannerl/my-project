8731b4f2d0d45214b987fdb66b72bd293d6d139a
bannerl <1761968987@qq.com>
2018-02-27 09:07:16 +0800

    edit apche

--- src/config/env.js
+++ src/config/env.js
@@ -12,8 +12,8 @@ if (process.env.NODE_ENV == 'development') {
     baseUrl = 'http://localhost:8080';
 
 }else if(process.env.NODE_ENV == 'production'){
-	baseUrl = 'http://localhost:8080';
-	//baseUrl = 'http://122.112.239.212:8080';
+	//baseUrl = 'http://localhost:8080';
+	baseUrl = 'http://106.14.169.113:8080';
 }
 
 export {
